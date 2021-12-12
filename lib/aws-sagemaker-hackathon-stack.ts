21786bd4064c12bd74314822a78377a390b4fc69
Spencer Fitzgerald <spencerfitz0196@gmail.com>
2021-12-11 22:23:34 -0700

    get stack working

--- lib/aws-sagemaker-hackathon-stack.ts
+++ lib/aws-sagemaker-hackathon-stack.ts
@@ -1,17 +1,40 @@
 import * as cdk from '@aws-cdk/core';
 import * as sagemaker from '@aws-cdk/aws-sagemaker';
-
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as s3 from '@aws-cdk/aws-s3'
+import * as iam from '@aws-cdk/aws-iam'
 
 export class AwsSagemakerHackathonStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'SagemakerArtifacts', {
+      bucketName: 'sagemaker-hackathon-sf96' //TODO: Edit Bucket Name
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsSagemakerHackathonQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const role = new iam.Role(this, 'JupyterNotebookRole', {
+      assumedBy: new iam.ServicePrincipal('sagemaker.amazonaws.com'),
+      description: 'Used by Jupyter Notebooks'
+    })
+
+    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSageMakerFullAccess'))
+    role.addToPolicy(new iam.PolicyStatement({
+      actions: [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:DeleteObject",
+        "s3:ListBucket"
+      ],
+      resources: [bucket.bucketArn],
+    }))
+
+    const notebooks = this.node.tryGetContext('notebooks')
+    // EC2 ML instance pricing: https://aws.amazon.com/sagemaker/pricing/
+    for (let notebook of notebooks) {
+      new sagemaker.CfnNotebookInstance(this, `Notebook-${notebook.instanceName}`, {
+        notebookInstanceName: notebook.instanceName,
+        instanceType: notebook.instanceType || 'ml.t3.medium', // EC2 ML instance pricing: https://aws.amazon.com/sagemaker/pricing/
+        roleArn: role.roleArn
+      })
+    }
   }
 }
