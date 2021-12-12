9d7ea19adb38c8255616cc76b61800c1c6cef1e1
Spencer Fitzgerald <spencerfitz0196@gmail.com>
2021-12-11 20:58:40 -0700

    Initial commit

---
+++ test/aws-sagemaker-hackathon.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as AwsSagemakerHackathon from '../lib/aws-sagemaker-hackathon-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-sagemaker-hackathon-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsSagemakerHackathon.AwsSagemakerHackathonStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
